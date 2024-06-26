19846d5511ef3af13fcfafcd38b527bc9dcb63bf
Andrea Pierré <andrea_pierre@brown.edu>
2024-06-25 20:56:49 -0400

    Rename & ignore slurm logs

--- TriangleTask/oscar/slurm_job.sh
+++ TriangleTask/oscar/slurm_job.sh
@@ -16,8 +16,8 @@
 #SBATCH --mail-type=begin       # send email when job begins
 #SBATCH --mail-type=end         # send email when job ends
 #SBATCH --mail-user=andrea_pierre@brown.edu
-#SBATCH -o slurm/stdout-%j.txt
-#SBATCH -e slurm/stderr-%j.txt
+#SBATCH -o slurm-logs/stdout-%j.txt
+#SBATCH -e slurm-logs/stderr-%j.txt
 ########### End Slurm header ##########
 
 # Load Slurm modules
