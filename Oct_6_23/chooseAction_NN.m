19791e34de45cf614c0514bd192f80dd05626425
Juan Mendez <jjm9@williams.edu>
2025-06-06 11:20:11 -0400

    Added comments to agent.py

--- Oct_6_23/chooseAction_NN.m
+++
@@ -1,7 +0,0 @@
-function [chosenAction] = chooseAction_NN(epsilon, actions, Q)
-%CHOOSEACTION Choose action based on action-selection method
-
-chosenAction = epsilonGreedy_NN(epsilon, actions, Q);
-
-end
-
--- cartesian_polar/agent.py
+++ cartesian_polar/agent.py
@@ -11,6 +11,16 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class EpsilonGreedy:
     """Define the Epsilon-greedy algorithm to chose an action."""
 
+    """
+    - The algorithm balances exploration and exploitation when choosing actions
+    
+    - 1. Generate random number
+    - 2. If number is less than epsilon, choose a random action (explore)
+    - 3. Otherwise, choose best action (highest Q-value)
+
+    - We decay epsilon over time, encouraging early exploration and late exploitation
+    """
+
     def __init__(
         self,
         epsilon,
@@ -30,6 +40,10 @@ class EpsilonGreedy:
         else:
             self.generator = None
 
+    """
+    action_space : space of possible actions
+    state_action_values : Q-values associated with each action in the action space
+    """
     def choose_action(self, action_space, state, state_action_values):
         """Choose an action a in the current world state (s)."""
 
@@ -42,19 +56,29 @@ class EpsilonGreedy:
         # Exploration
         if explor_exploit_tradeoff.item() < self.epsilon:
             # action = action_space.sample()
-            action = sample(action_space)
+            action = sample(action_space) # Random action from action space
 
         # Exploitation (taking the biggest Q-value for this state)
         else:
             # Break ties randomly
-            # If all actions are the same for this state we choose a random one
+            # If all actions have the same Q-values for this state we choose a random one
             # (otherwise `argmax()` would always take the first one)
             if torch.all(state_action_values == state_action_values[0]):
                 action = sample(action_space)
             else:
-                action = torch.argmax(state_action_values)
+                action = torch.argmax(state_action_values) # Take action with highest Q-value
         return action
 
+    """
+    Function to decay epsilon over time (episodes)
+
+    epsilon_warmup : number of episodes to wait before beginning decay
+    epsilon_min : a floor to prevent the agent becoming fully greedy and getting stuck in local optima
+
+    Decays according to classic exponential decay, a smooth reduction
+
+    !!! POINT OF EXPERIMENTATION !!! Play around with epsilon values and decay? What makes most sense for the mouse?
+    """
     def update_epsilon(self, ep):
         """Reduce epsilon after `ep` episodes."""
         if ep > self.epsilon_warmup:
@@ -69,13 +93,22 @@ class EpsilonGreedy:
         return epsilon
 
 
+"""Reinforcement Learning (DQN) Model Architecture"""
 class DQN(nn.Module):
     """Define network."""
 
+    """
+    n_observations -- sensory input, in this case Cue, Cartesian, and Polar coordinates
+    n_actions -- actions the agent can take, in this case Forward, Left, Right
+    n_units -- number of hidden neurons
+    """
     def __init__(self, n_observations, n_actions, n_units=16):
         super().__init__()
+        """
+        Network Architecture: 6 layers of neurons, 4 hidden layers
+        """
         self.mlp = nn.Sequential(
-            nn.Linear(n_observations, n_units),
+            nn.Linear(n_observations, n_units), 
             nn.Linear(n_units, n_units),
             nn.ReLU(),
             nn.Linear(n_units, n_units),
@@ -92,6 +125,13 @@ class DQN(nn.Module):
 
 def neural_network(n_observations, n_actions, nHiddenUnits):
     """Define policy and target networks."""
+
+    """
+    Policy Network : network agent uses to interact with environment and learn
+    Target Network : copy of policy, but updated less frequently
+                     - used to compute target Q-values, i.e. a stable snapshot I can use to estimate future rewards
+                     - helps stabilize training
+    """
     policy_net = DQN(
         n_observations=n_observations,
         n_actions=n_actions,
