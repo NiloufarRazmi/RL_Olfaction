9a76a6b803f7930ae48b5a0f6e1d92a1e71f26ea
Juan Mendez <jjm9@williams.edu>
2025-06-09 14:17:59 -0400

    Oscar test

--- cartesian_polar/slurm_job.sh
+++ cartesian_polar/slurm_job.sh
@@ -4,13 +4,13 @@
 # export PARAMSFILE="params.ini"; sbatch slurm_job.sh
 
 ########## Begin Slurm header ##########
-#SBATCH -p gpu --gres=gpu:1
+##SBATCH -p gpu --gres=gpu:1
+#SBATCH --partition=gpu-debug
 #SBATCH -N 1  # Number of node
 #SBATCH -n 2
 #SBATCH --mem=3G
 ##SBATCH -t 01:00:00
 #SBATCH -t 03:00:00
-##SBATCH --partition=gpu-debug
 #SBATCH --account=carney-afleisc2-condo
 #SBATCH --mail-type=begin       # send email when job begins
 #SBATCH --mail-type=end         # send email when job ends
@@ -24,8 +24,8 @@ module load python/3.11.0s-ixrhc3q
 module load texlive/20220321-pocclov
 
 # Run program
-. /users/jjmendez/data/jmendez/RL_Olfaction/cartesian_polar/.venv/bin/activate
-# pip install -Ue .
+. /users/jjmendez/data/jmendez/RL_Olfaction/cartesian_polar/.venv/bin/activate # Activate virtual environment
+pip install -Ue .
 # python ./TriangleTask/run_experiment.py
-# runexp $PARAMSFILE
-python run_experiment.py
+runexp $PARAMSFILE
+# python run_experiment.py
