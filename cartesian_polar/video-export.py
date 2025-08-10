6116d9bb60ad7f077293d9b44ed89717a0a0d97f
Juan Mendez <jjm9@williams.edu>
2025-08-09 22:09:43 -0400

    ci: fix lint

--- cartesian_polar/video-export.py
+++ cartesian_polar/video-export.py
@@ -1,16 +1,23 @@
-import cv2
+"""
+File for exporting frames to mp4 video.
+
+Frames are created using learning-animation.py.
+"""
+
 import os
 
+import cv2
+
 # Set the path to your frames
-frames_folder = './frames'
-output_video = 'output_video.mp4'
+frames_folder = "./frames"
+output_video = "output_video.mp4"
 fps = 3  # Change as needed; match FPS in learning-animation.py
 
 # Get a list of all frames
-frame_files = [f for f in os.listdir(frames_folder) if f.endswith(('.png', '.jpg'))]
+frame_files = [f for f in os.listdir(frames_folder) if f.endswith((".png", ".jpg"))]
 
 # Sort frames based on numbers in filename (assumes format like 'frame_001.png')
-frame_files = sorted(frame_files, key=lambda x: int(''.join(filter(str.isdigit, x))))
+frame_files = sorted(frame_files, key=lambda x: int("".join(filter(str.isdigit, x))))
 print(frame_files)
 
 # Read the first frame to get width and height
@@ -18,7 +25,7 @@ first_frame = cv2.imread(os.path.join(frames_folder, frame_files[0]))
 height, width, _ = first_frame.shape
 
 # Define the video writer
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # You can also try 'XVID' for .avi
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # You can also try 'XVID' for .avi
 video_writer = cv2.VideoWriter(output_video, fourcc, fps, (width, height))
 
 # Write frames to the video
